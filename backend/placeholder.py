ab83befb8304c8f8e72f8d1af4d2f31820d28c98
JustinChCa <bluestars12221@gmail.com>
2019-09-21 16:42:09 -0400

    test

--- backend/placeholder.py
+++ backend/placeholder.py
@@ -1,8 +1,10 @@
 from apiclient.discovery import build
 from static import API_KEY
 
+query = "business"
 youtube = build('youtube','v3', developerKey=API_KEY)
-req = youtube.search().list(part='snippet', q='business', type='channel')
+req = youtube.search().list(part='snippet', q=query, type='channel', maxResults=50)
 ref = req.execute()
 
-print(ref)
+for item in ref['items']:
+    print(item['snippet']['title'])
