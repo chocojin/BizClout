36ab282c2f8e1920f76b8a45f1ca92457047d46e
JustinChCa <bluestars12221@gmail.com>
2019-09-21 19:51:08 -0400

    new stuff

--- backend/placeholder.py
+++ backend/placeholder.py
@@ -1,10 +1,41 @@
 from apiclient.discovery import build
 from static import API_KEY
+import json
 
-query = "business"
-youtube = build('youtube','v3', developerKey=API_KEY)
-req = youtube.search().list(part='snippet', q=query, type='channel', maxResults=50)
-ref = req.execute()
+class Channel():
+    def __init__(self, channelId, title, **kwargs):
+        self.channelId = channelId
+        self.title = title
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
-for item in ref['items']:
-    print(item['snippet']['title'])
+def channelSC(query, maxResults, safeSearch, **kwargs):
+    """
+    Searches youtube for channels
+
+    Parameter query: Query to search for.
+    Precondition: string, length > 0
+
+    Parameter maxResults: How many channels to search for.
+    Preconditiion: int > 0
+    """
+    youtube = build('youtube', 'v3', developerKey=API_KEY)
+    req = youtube.search().list(part='snippet', q=query, type='channel',
+                                maxResults=maxResults, safeSearch=safeSearch)
+    ref = req.execute()
+    channelIds = []
+    for item in ref['items']:
+        channelIds.append(item['id']['channelId'])
+
+    channelIds = ','.join(channelIds)
+    req = youtube.channels().list(part='id, snippet, statistics',id=channelIds)
+    ref = req.execute()
+    channels = []
+    for item in ref['items']:
+        channels.append(Channel(item['id'], item['snippet']['title'],
+                        subscriberCount=item['statistics']['subscriberCount']))
+
+    return channels
+
+if __name__ == "__main__":
+    pass
